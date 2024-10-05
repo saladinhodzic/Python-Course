f45e7ec7cbb77bdd89376213906e7eada445f031
saladinhodzic <hsaladin06@gmail.com>
2024-10-05 21:44:26 +0200

    Simplyfing the code

--- Day2.py
+++ Day2.py
@@ -16,4 +16,8 @@ print(type(True))
 
 # Coding exercise
 
-print("Number of letters in your name: " + str(int(len(input("Enter your name")))))
+name_of_user=input("Enter name ")
+length_of_name=len(name_of_user)
+
+
+print("Number of letters in your name: " + str(length_of_name))
