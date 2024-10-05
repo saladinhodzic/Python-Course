676f42bdc3db6b53ce461096a4a6ccb82b287f71
saladinhodzic <hsaladin06@gmail.com>
2024-10-05 19:59:42 +0200

    Checking the type of data using type() function

---
+++ Day2.py
@@ -0,0 +1,16 @@
+# Integers- Celi brojevi
+print(123_345)
+
+# Float- Decimalni brojevi
+print(123.3)
+
+# Boolean
+print(True)
+
+# Checking type of data values
+
+print(type(123))
+print(type('123'))
+print(type(123.12))
+print(type(True))
+
