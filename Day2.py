490ba486d9b0fed4fa897f96f076118ae9982182
saladinhodzic <hsaladin06@gmail.com>
2024-10-06 12:55:57 +0200

    Mathematical operators in Python

--- Day2.py
+++ Day2.py
@@ -21,3 +21,9 @@ length_of_name=len(name_of_user)
 
 
 print("Number of letters in your name: " + str(length_of_name))
+
+# When dividing in Python using / you will always get float number
+# you can use // to get integer number
+
+print(6/2)
+print(6//2)
