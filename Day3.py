bb906cbdab580bd873bc99f6c33f41e9aea22a6a
saladinhodzic <hsaladin06@gmail.com>
2024-10-07 08:41:21 +0200

    Multiple if statements in succession

--- Day3.py
+++ Day3.py
@@ -10,18 +10,27 @@ else:
 #check if number is even or odd
 
 number=int(input('Enter number'))
+bill=0
 
 if number%2==0:
     print("Number is even")
     age=int(input('Enter your age'))
     
     if age<=12:
+        bill=7
         print('7$')
     elif age<18:
+        bill=10
         print('10$')
     else:
+        bill=12
         print("12$")    
 
+    photo=input('Photo or not')
 
+    if photo=='yes':
+        bill+=3
+
+    print(f"Total bill is {bill}")
 else:
     print("Number is odd")    
