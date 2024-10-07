0d9cd48d0a084a8ce5a9405f3b78d5ec38612f7b
saladinhodzic <hsaladin06@gmail.com>
2024-10-07 08:10:59 +0200

    Modulo operator

--- Day3.py
+++ Day3.py
@@ -6,3 +6,13 @@ if height>=120:
     print("You are okay")
 else:
     print("Grow taller")    
+
+#check if number is even or odd
+
+number=int(input('Enter number'))
+
+if number%2==0:
+    print("Number is even")
+else:
+    print("Number is odd")    
+    
