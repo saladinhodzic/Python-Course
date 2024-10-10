f6a74eafbd909a0ab019d219ca37d28b6ddce4b9
saladinhodzic <hsaladin06@gmail.com>
2024-10-10 20:16:13 +0200

    finding largest number in list building my method

--- Day5.py
+++ Day5.py
@@ -4,3 +4,24 @@ fruits=['apple','pear','cherry']
 
 for fruit in fruits:
     print(fruit)
+
+student_scores = [180, 124, 165, 173, 189, 169, 146]
+
+# print(sum(student_scores)) sum method for accumulating the result
+sum=0
+for score in student_scores:
+    sum+=score
+
+print(sum)
+
+# finding largest number in list
+
+# print(max(student_scores)) max method for finding largest number in list
+
+number=0
+
+for score in student_scores:
+    if score>number:
+        number=score
+
+print(number)
