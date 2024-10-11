bedb5ec6b196e32f5f5f6644cbb6013cb2034bf2
saladinhodzic <hsaladin06@gmail.com>
2024-10-11 14:30:00 +0200

    FizzBuzz challenge

--- Day5.py
+++ Day5.py
@@ -40,3 +40,15 @@ for number in range(1,101,1):
 
 
 print(zbir)
+
+for number in range(1,101,1):
+    if number%3==0 and number %5 ==0:
+        print("FizzBuzz")
+    elif number %5==0:
+        print("Buzz")
+        
+    elif number %3==0:
+        print("Fizz")
+        
+    else:
+        print(number)
