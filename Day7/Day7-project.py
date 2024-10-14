9aec1af21643fefb63b7a58eee3f4663cbf8a441
saladinhodzic <hsaladin06@gmail.com>
2024-10-14 07:49:41 +0200

    Getting user input

--- Day7/Day7-project.py
+++ Day7/Day7-project.py
@@ -5,14 +5,11 @@ words=['cat','cookie','sticks','paper','scissors','player','women','box']
 
 lives=6
 
-def guess():
-   input("Enter letter lowercase")
 
 def starting_game():
     random_word=random.choice(words)
+    user_guess=input("Enter letter lowercase: ")
     for letter in random_word:
-        user_guess= guess()
-        print(f"length is {len(random_word)}")
         if user_guess == letter:
             print("You got one")
         else:
