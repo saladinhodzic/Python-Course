bfa7e0c1a9f80cc5cb57fbb0898369cdd7b549e7
saladinhodzic <hsaladin06@gmail.com>
2024-10-17 11:59:17 +0200

    Now it works better just need to make sure when index is negative to change it

--- Day8/Day8_ceaser.py
+++ Day8/Day8_ceaser.py
@@ -1,15 +1,28 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 def encrypt():
-    if direction == "encode":
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
         encrypt_text=''
         for letter in text:
             index=alphabet.index(letter)
             encrypt_text+=alphabet[index-shift]
-        print(encrypt_text)
+        print(encrypt_text)   
+def decrypt():
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        decrypt_text=''
+        for letter in text:
+            index=alphabet.index(letter)
+            decrypt_text+=alphabet[index+shift]
+        print(decrypt_text)        
 
-encrypt()
+if direction == 'encode':
+      encrypt()
+      decode=input("Do you want to decode word? y or n")
+      if decode == "y":
+        decrypt()
+else:
+      decrypt()
