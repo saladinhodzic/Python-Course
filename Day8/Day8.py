8ee8457ad446cd2278f07a7c9752ad8ac9ad6d4b
saladinhodzic <hsaladin06@gmail.com>
2024-10-17 11:44:41 +0200

    Ceaser Cipher works but its still not done

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -7,5 +7,7 @@ def greet_with(name,location):
     print(f"Hello {name}")
     print(f"How is it in {location}")
 
+# positional arguments
 greet_with('saladin','novi pazar')
+# keyword arguments
 greet_with(location="Novi pazar",name="saki")
---
+++ Day8/Day8_ceaser.py
@@ -0,0 +1,15 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt():
+    if direction == "encode":
+        encrypt_text=''
+        for letter in text:
+            index=alphabet.index(letter)
+            encrypt_text+=alphabet[index-shift]
+        print(encrypt_text)
+
+encrypt()
