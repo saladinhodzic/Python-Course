d1651f7847b4c758d2baa474c30402331cb91998
saladinhodzic <hsaladin06@gmail.com>
2024-10-17 10:58:07 +0200

    Function taking multiple parameters

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -2,3 +2,9 @@ def greet (name):
     print(name)
 
 greet("Saladin")
+
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"How is it in {location}")
+
+greet_with('saladin','novi pazar')
