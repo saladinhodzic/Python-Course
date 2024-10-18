73becace3d78a6ea481a0e6a967cacdc0e810d87
saladinhodzic <hsaladin06@gmail.com>
2024-10-18 18:44:06 +0200

    Cleaning the code even more

--- Day8/Day8_ceaser.py
+++ Day8/Day8_ceaser.py
@@ -5,24 +5,17 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 def ceaser(direction,text,shift):
-        if direction == 'encode':
                 encrypt_text=''
+                if direction == 'decode':
+                        shift*=-1
                 for letter in text:
-                         if letter not in alphabet:
+                    if letter not in alphabet:
                            encrypt_text+=letter
-                         else:
-                           index=alphabet.index(letter)
-                           encrypt_text+=alphabet[index+shift]
-                print(encrypt_text)  
-        else:
-                decrypt_text=''
-                for letter in text:
-                   if letter not in alphabet:
-                           decrypt_text+=letter
-                   else:
+                    else:
                         index=alphabet.index(letter)
-                        decrypt_text+=alphabet[index-shift]
-                print(decrypt_text) 
+                        encrypt_text+=alphabet[index+shift]
+                print(encrypt_text)  
+                
 
 ceaser(direction,text,shift)           
 
