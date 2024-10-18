fb6916f623d95fe13286bf5002daa48e48f4d6fb
saladinhodzic <hsaladin06@gmail.com>
2024-10-18 17:46:45 +0200

    Putting it all inside one function

--- Day8/Day8_ceaser.py
+++ Day8/Day8_ceaser.py
@@ -2,33 +2,29 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 
-def encrypt():
-        text = input("Type your message:\n").lower()
-        shift = int(input("Type the shift number:\n"))
-        encrypt_text=''
-        for letter in text:
-            if letter not in alphabet:
-                encrypt_text+=letter
-            else:
-                index=alphabet.index(letter)
-                encrypt_text+=alphabet[index+shift]
-        print(encrypt_text)   
-def decrypt():
-        text = input("Type your message:\n").lower()
-        shift = int(input("Type the shift number:\n"))
-        decrypt_text=''
-        for letter in text:
-           if letter not in alphabet:
-                decrypt_text+=letter
-           else:
-                index=alphabet.index(letter)
-                decrypt_text+=alphabet[index-shift]
-        print(decrypt_text)        
+def ceaser(direction):
+        if direction == 'encode':
+                text = input("Type your message:\n").lower()
+                shift = int(input("Type the shift number:\n"))
+                encrypt_text=''
+                for letter in text:
+                         if letter not in alphabet:
+                           encrypt_text+=letter
+                         else:
+                           index=alphabet.index(letter)
+                           encrypt_text+=alphabet[index+shift]
+                print(encrypt_text)  
+        else:
+                text = input("Type your message:\n").lower()
+                shift = int(input("Type the shift number:\n"))
+                decrypt_text=''
+                for letter in text:
+                   if letter not in alphabet:
+                           decrypt_text+=letter
+                   else:
+                        index=alphabet.index(letter)
+                        decrypt_text+=alphabet[index-shift]
+                print(decrypt_text) 
 
-if direction == 'encode':
-      encrypt()
-      decode=input("Do you want to decode word? y or n")
-      if decode == "y":
-        decrypt()
-else:
-      decrypt()
+ceaser(direction)           
+
