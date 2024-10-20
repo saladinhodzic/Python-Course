313f155fc3c1d98a213b76b06540842114be364a
saladinhodzic <hsaladin06@gmail.com>
2024-10-20 19:55:06 +0200

    Improving the code

--- Day8/Day8_ceaser.py
+++ Day8/Day8_ceaser.py
@@ -3,7 +3,6 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-
 def ceaser(direction,text,shift):
                 encrypt_text=''
                 if direction == 'decode':
@@ -16,6 +15,11 @@ def ceaser(direction,text,shift):
                         encrypt_text+=alphabet[index+shift]
                 print(encrypt_text)  
                 
+ceaser(direction,text,shift)                  
+restart=input('Do you want to go again?')
 
-ceaser(direction,text,shift)           
+if restart=='yes':
+        ceaser(direction,text,shift)        
+
+          
 
