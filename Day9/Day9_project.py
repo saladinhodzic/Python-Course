3144baea5a75a4d1c6b89139272f20c8ab051f2a
saladinhodzic <hsaladin06@gmail.com>
2024-10-23 23:23:33 +0200

    Starting the bidding auction project

---
+++ Day9/Day9_project.py
@@ -0,0 +1,6 @@
+bidders={}
+print("Start bidding")
+first_user_name=input("Enter your name ")
+first_user_bid=int(input("Enter your bidding value "))
+bidders[first_user_name]=first_user_bid
+print(bidders)
