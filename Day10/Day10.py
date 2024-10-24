bdcfe24758fef233d8480ccb3c311e138427ded9
saladinhodzic <hsaladin06@gmail.com>
2024-10-24 20:05:11 +0200

    Early return and multiple returns in function

--- Day10/Day10.py
+++ Day10/Day10.py
@@ -1,6 +1,8 @@
 # creating functions with outputs
 
 def format_name(first,last):
+    if first=='' or last== '':
+        return 'You entered invalid value'
     return f"{first} {last}".title()
 
-print(format_name("saki",'hodzic'))
+print(format_name(input("enter first name"),input('enter last name')))
