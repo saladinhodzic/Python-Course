e9a4f4b4c1872dc88891b8af9550ddd85e6bd6c7
saladinhodzic <hsaladin06@gmail.com>
2024-10-25 10:51:20 +0200

    Introducing docstrings

--- Day10/Day10.py
+++ Day10/Day10.py
@@ -1,6 +1,8 @@
 # creating functions with outputs
 
 def format_name(first,last):
+    """This is example of docstring which can be used to
+    give information about the function and can be written multi line"""
     if first=='' or last== '':
         return 'You entered invalid value'
     return f"{first} {last}".title()
