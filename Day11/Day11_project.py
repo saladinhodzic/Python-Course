4d9a0dacebc6faa48068e6865afc6d1826985517
saladinhodzic <hsaladin06@gmail.com>
2024-10-26 19:43:22 +0200

    Adding two random cards to my cards

--- Day11/Day11_project.py
+++ Day11/Day11_project.py
@@ -1,2 +1,15 @@
 # Blackjack
+import random
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+dealer_cards=[]
+my_cards=[]
 
+wanna_play=input("Do you want to play Blackjack y or n? ")
+
+def draw_cards():
+    for card in range(2):
+        my_cards.append(random.choice(cards))
+    print(my_cards)
+
+if wanna_play == 'y':
+    draw_cards()
