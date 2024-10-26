b46a5e27bc56df20ccdcdcfc91b207d02cd4b836
saladinhodzic <hsaladin06@gmail.com>
2024-10-26 19:44:28 +0200

    Showing only one dealer card

--- Day11/Day11_project.py
+++ Day11/Day11_project.py
@@ -9,7 +9,9 @@ wanna_play=input("Do you want to play Blackjack y or n? ")
 def draw_cards():
     for card in range(2):
         my_cards.append(random.choice(cards))
+        dealer_cards.append(random.choice(cards))
     print(my_cards)
+    print(dealer_cards[0])
 
 if wanna_play == 'y':
     draw_cards()
