3edf703c3355e945d01d1db0ee5407ae7af14657
saladinhodzic <hsaladin06@gmail.com>
2024-11-01 16:14:26 +0100

    It works for most times maybe i missed something

--- Day14/Day14.py
+++ Day14/Day14.py
@@ -4,18 +4,35 @@ import random
 
 score = 0
 
-a=random.choice(data)
-b=random.choice(data)
-print(a['name'])
+is_playing=True
 
-print(b['name'])
+while is_playing==True:
 
-a_follower_count=a["follower_count"]
-b_follower_count=b["follower_count"]
+    a=random.choice(data)
+    b=random.choice(data)
 
 
-lower_or_higher=input("Which of these two are more popular 'A' or 'B': ")
+    a_follower_count=a["follower_count"]
+    b_follower_count=b["follower_count"]
 
-if lower_or_higher=='A':
-    if a_follower_count>b_follower_count:
-        print("Bingo")
+    print(a['name'])
+    print(b['name'])
+
+    lower_or_higher=input("Which of these two are more popular 'A' or 'B': ")
+
+    if lower_or_higher=='A':
+        if a_follower_count>b_follower_count:
+            score+=1
+            print("Bingo")
+            print(f"Your score is {score}")
+        else:
+            print(f'You lost! Your score is {score}')
+            is_playing=False
+    if lower_or_higher=='B':
+        if  b_follower_count>a_follower_count:
+            score+=1
+            print("Bingo")
+            print(f"Your score is {score}")
+        else:
+            print(f'You lost! Your score is {score}')
+            is_playing=False
