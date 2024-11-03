76f7a477e772fe2f425118015dcc9ee8918d3694
saladinhodzic <hsaladin06@gmail.com>
2024-11-03 23:52:17 +0100

    Adding TODOs

--- Day15/project.py
+++ Day15/project.py
@@ -1,1 +1,21 @@
-from data import MENU 
+from data import MENU,resources
+
+# TODO - ask user for coffee
+
+
+order=input("What would you like? (espresso/latte/cappuccino): ")
+
+# TODO - on saying word 'off' it should end the program
+
+
+# TODO - on saying word 'report' it should print the coffee machine current resources
+
+
+# TODO - check if coffee machine has enough resources for making the user coffee
+
+
+# TODO - If it has enough resources then it should ask for coins (penny, dimes, nickel, quarters)
+
+# TODO - Check if user inserted enough money, if it has too much money then it should refund
+
+# TODO - Deduce the resources from coffe machine and wish the user happy day
