3a991799d10cc7ac26196503841f86cd72a56b7b
saladinhodzic <hsaladin06@gmail.com>
2024-11-04 00:39:18 +0100

    Check if coffee is espresso then skip the statement with milk

--- Day15/project.py
+++ Day15/project.py
@@ -9,6 +9,20 @@ def ask_for_drink():
 is_off=False
 # TODO - on saying word 'off' it should end the program
 
+def check_resources(resources,user_coffee):
+        if resources['water'] < user_coffee['ingredients']['water']:
+            print("There is not enough water in machine!")
+        elif resources['coffee'] < user_coffee['ingredients']['coffee']:
+            print("There is not enough coffee in machine!")
+        elif user_coffee==MENU['espresso']:
+            print("Espresso is on the way!")
+            return
+        elif resources['milk'] < user_coffee['ingredients']['milk']:
+            print("There is not enough milk in machine!")
+        else:
+            print("Coffe is on the way!")
+            return
+
 order=ask_for_drink()
 while is_off==False:
     if order =='off':
@@ -16,6 +30,12 @@ while is_off==False:
         is_off=True
     elif order == 'report':
         print(resources)
+        order=ask_for_drink()
+    elif order == 'espresso':
+        check_resources(resources,MENU['espresso'])
+        order=ask_for_drink()
+    elif order == 'cappuccino':
+        check_resources(resources,MENU['cappuccino'])
         order=ask_for_drink()
 
 
