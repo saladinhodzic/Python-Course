909e9a89abf1f34b7041efded89889674d2d99f8
saladinhodzic <hsaladin06@gmail.com>
2024-11-07 21:06:42 +0100

    Completing more of the lab exercises

--- lab-vezbe/lab-vezbe.py
+++ lab-vezbe/lab-vezbe.py
@@ -3,8 +3,35 @@
 # 1. Napisati program u kojem korisnik unosi broj godina radnog staža i ispisuje "Ispunjavaš uslov
 # za napredovanje." ako je staž veći od 5 godina, u suprotnom ispisuje "Potrebno je još iskustva."
 
-staz=int(input("Unesi godine radnog staza"))
-if staz > 5:
-    print("Ispunjavas uslov za napredovanje")
+# staz=int(input("Unesi godine radnog staza"))
+# if staz > 5:
+#     print("Ispunjavas uslov za napredovanje")
+# else:
+#     print("Potrebno je jos iskustva ")
+
+# 2. Napisati program u kojem korisnik unosi email adresu i ispisuje "Validan email!" ako adresa
+# sadrži karakter "@", u suprotnom ispisuje "Nevalidan email."
+
+# email=input("unesite email adresu")
+# if '@' in email:
+#     print("Validan email")
+# else:
+#     print("Email nije validan")
+
+# 3. Napisati program u kojem korisnik unosi lozinku i ispisuje "Lozinka je jaka!" ako je dužina
+# lozinke veća od 8 karaktera, u suprotnom "Lozinka je prekratka."
+
+# lozinka=input("Unesite lozinku")
+# if len(lozinka)>8:
+#     print("Lozinka je okej")
+# else:
+#     print("Prekratko")
+
+# 4. Napisati program u kojem korisnik unosi dan u nedelji i ispisuje "Vikend!" ako je dan subota
+# ili nedelja, u suprotnom ispisuje "Radni dan."
+
+dan=input("Unesi dan")
+if dan == 'nedelja' or dan == 'subota':
+    print("Neradan dan")
 else:
-    print("Potrebno je jos iskustva ")
+    print("radan")
