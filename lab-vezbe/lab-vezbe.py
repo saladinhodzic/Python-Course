6bd2acb64847adced3f909f486c6e577f22cc90a
saladinhodzic <hsaladin06@gmail.com>
2024-11-08 17:29:59 +0100

    Happy number

--- lab-vezbe/lab-vezbe.py
+++ lab-vezbe/lab-vezbe.py
@@ -42,13 +42,54 @@
 # 1
 # 3+53+33=153.
 
-def is_armstrong_number(input):
-    a=int(input[0])
-    b=int(input[1])
-    c=int(input[2])
-    if a**3 + b**3 + c**3 == int(input):
-        print("Its armstrong number")
-    else:
-        print("its not armstrong number")
+# def is_armstrong_number(input):
+#     a=int(input[0])
+#     b=int(input[1])
+#     c=int(input[2])
+#     if a**3 + b**3 + c**3 == int(input):
+#         print("Its armstrong number")
+#     else:
+#         print("its not armstrong number")
     
-is_armstrong_number(input("Unesi trocifren broj"))
+# is_armstrong_number(input("Unesi trocifren broj"))
+
+# 6. Napisati program u kojem korisnik unosi dva broja X i N. Program generiše sve parove celih
+# brojeva (a, b) gde je a < b, a zbir a + b jednak je X. Ispisati sve takve parove.
+
+# x=int(input("unesi prvi broj"))
+# n=int(input("unesi drugi broj"))
+
+# def is_equal_to_X(x,n):
+#     for a in range(1,n):
+#         for b in range(n,0,-1):
+#             if a<b and a + b ==x:
+#                 print(f"{a},{b}")
+# is_equal_to_X(x,n)
+
+# 7. Napisati program u kojem korisnik unosi reč i ispisuje "Duga reč" ako je dužina reči veća od 5
+# karaktera, u suprotnom "Kratka reč."
+
+# rec=input("Napisi rec")
+
+# if len(rec)>5:
+#     print("Duga rec")
+# else:
+#     print("Kratka rec")
+
+# 8 Napisati program u kojem korisnik unosi broj N. Program koristi while petlju za generisanje
+# brojeva od 1 do N i proverava da li su "brojevi sreće" (brojevi gde zbir kvadrata njihovih cifara
+# na kraju dovodi do 1).
+
+N=int(input("Unesi broj"))
+i=1
+while i<=N:
+    novi=str(i)
+    list=[]
+    is_happy=0
+    for char in novi:
+        list.append(int(char))
+    for num in list:
+        is_happy+=num**2
+    if is_happy==1:
+        print(novi)
+    i+=1
