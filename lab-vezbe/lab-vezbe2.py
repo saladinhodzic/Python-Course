83ae8e4ccf1fb41a2d3851ba732549ad07441c93
saladinhodzic <hsaladin06@gmail.com>
2024-12-06 10:10:36 +0100

    Checking how many times same element occurs in a list

---
+++ lab-vezbe/lab-vezbe2.py
@@ -0,0 +1,23 @@
+# Korisnik unosi listu reči odvojenih razmakom, a program treba da izračuna koliko se puta
+# svaka reč pojavljuje u listi, pronađe najdužu reč i izdvoji sve reči koje su palindromi. Na
+# primer, za ulaznu listu ["ana", "banana", "ana", "kajak", "noc"], program ispisuje broj
+# pojavljivanja reči ("ana: 2 puta", "banana: 1 puta", itd.), najdužu reč ("banana") i sve
+# palindromske reči ("ana, kajak"). Ako u listi nema palindroma, ispisuje se poruka "Nema
+# palindromskih reči".
+
+
+lista=[]
+length=int(input("Unesite duzinu niza"))
+
+for i in range(length):
+    lista.append(input("Unesite element: "))
+
+# provera koliko se puta ispisuju date reci
+check={}
+for rec in lista:
+    brojac=0
+    for ista_rec in lista:
+        if rec == ista_rec:
+            brojac+=1
+            check[rec]=brojac
+print(check)
