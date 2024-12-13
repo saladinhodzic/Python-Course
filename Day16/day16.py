7dc4f28e63f6ff5d4f0844e3d3d9d48f9cf723b9
saladinhodzic <hsaladin06@gmail.com>
2024-12-13 17:32:12 +0100

    Working with module classes (prettytable)

--- Day16/day16.py
+++ Day16/day16.py
@@ -1,9 +1,17 @@
 # Object Oriented Programing
 
-from turtle import Turtle,Screen
+# from turtle import Turtle,Screen
 
-rafaelo=Turtle()
-rafaelo.shape("turtle")
-rafaelo.color("green")
-rafaelo.forward(50)
-Screen().exitonclick()
+# rafaelo=Turtle()
+# rafaelo.shape("turtle")
+# rafaelo.color("green")
+# rafaelo.forward(50)
+# Screen().exitonclick()
+
+from prettytable import PrettyTable
+
+table=PrettyTable()
+table.add_column("Makarone",["Pesto", "Milano","Americana"])
+table.add_column("Kafe",["Espresso", "Macchiato","Capuccino"])
+table.align='l'
+print(table)
