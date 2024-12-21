e0055c9a62ff4904377fa096fc05c71162b8d1dc
saladinhodzic <hsaladin06@gmail.com>
2024-12-21 17:02:42 +0100

    Add list loops exercise for even numbers and perfect number check

---
+++ exercises/list-loops.py
@@ -0,0 +1,31 @@
+# LISTS LOOPS
+
+# Napisati program u programskom jeziku Python koji
+# ispisuje sve parne brojeve između 1 i 20
+
+# for i in range(1,21):
+#     if i % 2 == 0:
+#         print(i)
+        
+# i=1
+
+# while i < 21:
+#     if i % 2 == 0:
+#         print(i)
+#     i+=1
+
+# Savršen broj je broj koji je jednak zbiru svih svojih delilaca
+# (osim samog sebe). Na primer, broj 6 je savršen jer su
+# njegovi delioci 1, 2 i 3, a zbir 1 + 2 + 3 = 6. Napisati
+# program koji proverava da li je uneti broj savršen.
+
+# broj = int(input("Unesite neki broj "))
+# acc=0
+# for i in range(1,broj):
+#     if broj % i ==0:
+#         acc+=i
+
+# if acc == broj:
+#     print("Broj je savrsen")
+# else:
+#     print("Broj nije savrsen")
