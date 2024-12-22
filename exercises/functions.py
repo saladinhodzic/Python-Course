9c60c906ffd5b1c71e6a3b8414e2cc0f3f5f0159
saladinhodzic <hsaladin06@gmail.com>
2024-12-22 15:55:30 +0100

    Add number guessing game implementation and power function

--- exercises/functions.py
+++ exercises/functions.py
@@ -49,15 +49,50 @@
 # Pozove funkciju ping(i-1) ako je i > 0.
 # Vraća "1" ako je i == 0.
 
-def ping(i):
-    if i>0:
-        return pong(i-1)
-    if i == 0:
-        return 0
-def pong(i):
-    if i > 0:
-        return ping(i-1)
-    if i==0:
-        return 1
+# def ping(i):
+#     if i>0:
+#         return pong(i-1)
+#     if i == 0:
+#         return 0
+# def pong(i):
+#     if i > 0:
+#         return ping(i-1)
+#     if i==0:
+#         return 1
 
-print(ping(5))
+# print(ping(5))
+
+# Sastaviti funkciju za stepenovanje brojeva. Zatim napisati program u
+# programskom jeziku Python koji pomaže formirane funkcije vrši računanje za
+# dati broj i stepen i ispisuje rezultat.
+
+# def stepen(broj,stepen):
+#     return broj ** stepen
+# print(stepen(3,3))
+
+'''Napisati program koji implementira igricu ”Pogodi broj”.
+Na početku igre računar zamišlja jedan slučajan broj u
+intervalu [0,100]. Nakon toga igrač unosi svoje ime i
+započinje igru. Igrač unosi jedan po jedan broj sve dok ne
+pogodi koji broj je računar zamislio. Svaki put kada igrač
+unese broj, u zavisnosti od toga da li je broj koji je unet
+veći ili manji od zamišljenog broja ispisuje se
+odgovarajuća poruka. Igra se završava u trenutku kada
+igrač pogodio zamišljen broj.'''
+
+import random
+def pogodi_broj():
+    zamisljen_broj=random.randint(0,100)
+    ime=input("Unesite vase ime: ")
+    print("Vase ime je",ime)
+    while True:
+        nas_broj=int(input("Unesite broj "))
+        if nas_broj>zamisljen_broj:
+            print("Manje")
+        elif nas_broj<zamisljen_broj:
+            print("Vise")
+        else:
+            print("Pogodili ste broj!")
+            break
+        
+pogodi_broj()
