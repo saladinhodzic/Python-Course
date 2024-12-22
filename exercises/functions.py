9ad275989341eee6de49c3472c4f95616c1209ba
saladinhodzic <hsaladin06@gmail.com>
2024-12-22 09:43:52 +0100

    Add function to print name and age from user input

---
+++ exercises/functions.py
@@ -0,0 +1,10 @@
+# FUNCTIONS, MODULES, LIST COMPREHENSION
+
+# Na programskom jeziku Python, napišite funkciju
+# koja ispisuje poruku u formatu "Ime: [ime],
+# Godine: [godine]". Vrednosti za ime i godine
+# treba da se unesu preko ulaza.
+
+def poruka(ime,godine):
+    print("Ime:",ime,"\nGodine:",godine)
+poruka("Saladin",18)
