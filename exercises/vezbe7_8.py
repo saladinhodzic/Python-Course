98ea8d583b8f84bee747f1b2ce05accaab3c4bde
saladinhodzic <hsaladin06@gmail.com>
2024-12-23 13:31:24 +0100

    Add program to create a new list from the maximum elements of two input lists

--- exercises/vezbe7_8.py
+++ exercises/vezbe7_8.py
@@ -32,12 +32,46 @@
 # niz ulevo za k pozicija. Na primer, ako je niz [1, 2, 3, 4, 5] i
 # k = 2, rezultat bi bio [3, 4, 5, 1, 2]
 
-niz = input("Unesite brojeve niza\n").split(" ")
+# niz = input("Unesite brojeve niza\n").split(" ")
 
-niz=[int(x) for x in niz]
+# niz=[int(x) for x in niz]
 
-k= int(input("Unesite broj pozicija za rotaciju: "))
+# k= int(input("Unesite broj pozicija za rotaciju: "))
 
-rotiran_niz= niz[k:] + niz[:k]
+# rotiran_niz= niz[k:] + niz[:k]
 
-print(rotiran_niz)
+# print(rotiran_niz)
+
+'''Na programskom jeziku Pythonu sastaviti program
+koji od dva niza celih brojeva iste dužine formira
+treći niz tako da bude zadovoljen uslov: c[i] =
+max(a[i], b[i]). Program treba da sa glavnog ulaza
+da učita dužinu nizova N, gde je 𝑁<200, da
+omogući unos elemenata dva niza, da formira i
+ispiše rezultujući niz C, da koristi funkcije za:unos
+niza,obradu (formiranje rezultujućeg niza),ispis
+rezultata. Program se ponavlja sve dok korisnik ne
+unese nevalidnu vrednost za 𝑁(npr. 𝑁≥200 ili 𝑁≤0).'''
+
+def unos(n):
+    niz=[]
+    for _ in range(n):
+        broj = int(input(f"Unesite {_+1}. broj niza "))
+        niz.append(broj)
+    return niz
+
+def formiraj(prvi,drugi,n):
+    novi_niz=[]
+    for i in range(n):
+        novi_niz.append(max(prvi[i],drugi[i]))
+    return novi_niz
+while True:
+    n=int(input("Unesite duzinu dva niza: "))
+    if n>=200 or n<=0:
+        break
+    
+    prvi_niz=unos(n)
+    drugi_niz=unos(n)
+    
+    novi = formiraj(prvi_niz,drugi_niz,n)
+    print(novi)
