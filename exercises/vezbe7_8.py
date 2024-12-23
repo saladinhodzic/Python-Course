ca618c91ccd9d47de35877224bbbd629c43e5e57
saladinhodzic <hsaladin06@gmail.com>
2024-12-23 12:58:09 +0100

    Add program to rotate a list to the left by a specified number of positions

--- exercises/vezbe7_8.py
+++ exercises/vezbe7_8.py
@@ -11,8 +11,33 @@
 # Na programskom jeziku Python sastaviti program
 # koji računa zbir svih elemenata niza.
 
-niz = input("Unesite brojeve: ").split(" ")
-suma=0
-for broj in niz:
-    suma+=int(broj)
-print(suma)
+# niz = input("Unesite brojeve: ").split(" ")
+# suma=0
+# for broj in niz:
+#     suma+=int(broj)
+# print(suma)
+
+# Na programskom jeziku Python sastaviti program
+# koji sa standardnog ulaza unosi elemenata niza i
+# sortira niz u rastućem redosledu
+
+# niz = input("Unesite brojeve niza\n").split(" ")
+
+# novi_niz=[int(broj) for broj in niz]
+
+# novi_niz.sort()
+# print(novi_niz)
+
+# Na programskom jeziku Python sastaviti program koji rotira
+# niz ulevo za k pozicija. Na primer, ako je niz [1, 2, 3, 4, 5] i
+# k = 2, rezultat bi bio [3, 4, 5, 1, 2]
+
+niz = input("Unesite brojeve niza\n").split(" ")
+
+niz=[int(x) for x in niz]
+
+k= int(input("Unesite broj pozicija za rotaciju: "))
+
+rotiran_niz= niz[k:] + niz[:k]
+
+print(rotiran_niz)
