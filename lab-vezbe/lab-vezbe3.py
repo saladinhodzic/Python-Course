05c5acec4b6e3356d31eb964674f913f76e303bd
saladinhodzic <hsaladin06@gmail.com>
2024-12-29 16:24:04 +0100

    Add program to convert hexadecimal number to decimal with input validation

--- lab-vezbe/lab-vezbe3.py
+++ lab-vezbe/lab-vezbe3.py
@@ -38,12 +38,36 @@ standardnom izlazu ispiše indekse svih onih elemenata niza za koje važi da pre
 aritmetičku sredinu prethodnog i sledećeg elementa. Drugim rečima, za svako i=2,...,n−1
 za koje važi da je a[i]=(a[i−1]+a[i+1])/2, program treba da ispiše vrednost i. '''
 
-n = int(input("Unesite pozitivnu duzinu n: "))
-niz=[]
-for i in range(n):
-    broj = float(input("Unesite element niza: "))
-    niz.append(broj)
+# n = int(input("Unesite pozitivnu duzinu n: "))
+# niz=[]
+# for i in range(n):
+#     broj = float(input("Unesite element niza: "))
+#     niz.append(broj)
 
-for i in range(1,n-1):
-    if niz[i] == (niz[i-1] + niz[i+1])/2:
-        print(i)
+# for i in range(1,n-1):
+#     if niz[i] == (niz[i-1] + niz[i+1])/2:
+#         print(i)
+
+'''4. Napisati program na programskom jeziku Python koji računa i ispisuje decimalnu
+vrednost neoznačenog heksadecimalnog broja koji se učitava sa glavnog ulaza kao niz
+znakova od najviše 6 karaktera. Pre računanja vrednosti, program treba da izvrši proveru
+da li se u nizu nalaze samo dozvoljeni znakovi (cifre 0..9 i znakovi A..F). Ukoliko se u
+nizu nalaze nedozvoljeni znakovi, ispisati odgovarajuću poruku i prekinuti izvršavanje. U
+suprotnom, ispisati izračunatu decimalnu vrednost.'''
+
+def decimal():
+    niz = input("Unesite heksadecimalni broj(najvise do 6 cifara): ").strip()
+    hex = "0123456789ABCDEF"
+    if len(niz)>6:
+        print("Uneli ste predugacak broj!")
+        return 
+    for znak in niz.upper():
+        if znak not in hex:
+            print("Niste uneli validne vrednosti!")
+            return
+    decimal=0
+    for i,znak in enumerate(reversed(niz)):
+        vrednost = int(znak,16)
+        decimal += vrednost * (16 ** i)
+    return decimal
+print(decimal())
