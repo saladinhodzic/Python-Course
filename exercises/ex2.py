fb848c92b2cee9383ced024004ce6ec1c006f3c4
saladinhodzic <hsaladin06@gmail.com>
2024-12-31 17:40:41 +0100

    Add number expansion calculator program

--- exercises/ex2.py
+++ exercises/ex2.py
@@ -16,5 +16,17 @@ Write a Python program that accepts a filename from the user and prints the exte
 Sample filename : abc.java
 Output : java'''
 
-file_name = input("Enter file name: ").split(".")
-print(file_name[1])
+# file_name = input("Enter file name: ").split(".")
+# print(file_name[1])
+
+'''10. Number Expansion Calculator
+
+Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.
+Sample value of n is 5
+Expected Result : 615'''
+
+n = int(input("Enter number for expansion: "))
+
+acc= n + (n*10+n) + (n*100 + n*10 + n)
+
+print(acc)
