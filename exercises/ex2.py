b484c50e8be6e26e94b1c0724fae6a8e7b5626e6
saladinhodzic <hsaladin06@gmail.com>
2024-12-31 17:36:14 +0100

    Add file extension extractor program and remove commented-out code

--- exercises/ex2.py
+++ exercises/ex2.py
@@ -6,6 +6,15 @@
 # List : ['3', ' 5', ' 7', ' 23']
 # Tuple : ('3', ' 5', ' 7', ' 23')
 
-numbers = input("Enter sequence of numbers comma separated: ").split(", ")
-torka = tuple(numbers)
-print(numbers,torka)
+# numbers = input("Enter sequence of numbers comma separated: ").split(", ")
+# torka = tuple(numbers)
+# print(numbers,torka)
+
+'''7. File Extension Extractor
+
+Write a Python program that accepts a filename from the user and prints the extension of the file.
+Sample filename : abc.java
+Output : java'''
+
+file_name = input("Enter file name: ").split(".")
+print(file_name[1])
