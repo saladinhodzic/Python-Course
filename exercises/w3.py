a72b39e6a86e2c3005811a674731cb5f7898042f
saladinhodzic <hsaladin06@gmail.com>
2025-01-28 20:52:39 +0100

    Add functionality to check list length and fifth element occurrences

--- exercises/w3.py
+++ exercises/w3.py
@@ -2,19 +2,29 @@
 
 # Write a Python program to find a list of integers with exactly two occurrences of nineteen and at least three occurrences of five. Return True otherwise False.
 
-lst = [1,19,19,5,5]
-brojac_19 = 0
-brojac_5=0
-for broj in lst:
-    if broj == 19:
-        brojac_19+=1
-    elif broj == 5:
-        brojac_5 +=1
-if brojac_19 == 2 and brojac_5 >=3:
-    print("True")
-else:
-    print("False")
+# lst = [1,19,19,5,5]
+# brojac_19 = 0
+# brojac_5=0
+# for broj in lst:
+#     if broj == 19:
+#         brojac_19+=1
+#     elif broj == 5:
+#         brojac_5 +=1
+# if brojac_19 == 2 and brojac_5 >=3:
+#     print("True")
+# else:
+#     print("False")
 # if lst.count(19) == 2 and lst.count(5) >=3:
 #     print("True")
 # else:
 #     print("False")
+
+# 2. Fifth Element and List Length Check
+
+# Write a Python program that accepts a list of integers and calculates the length and the fifth element. Return true if the length of the list is 8 and the fifth element occurs thrice in the said list.
+
+lst = [1,2,3,4,5,5,5,6]
+if len(lst) == 8 and lst.count(lst[4]) == 3:
+    print("True")
+else:
+    print("False")
