b40e44ca8b8818a2cad77ddccc8bf56ae6679b07
saladinhodzic <hsaladin06@gmail.com>
2025-01-29 19:08:42 +0100

    Refactor turtle drawing to create a circular pattern with dynamic color changes

--- Day18/Day18.py
+++ Day18/Day18.py
@@ -2,20 +2,22 @@ import turtle
 import random
 turtle.colormode(255)
 nindza = turtle.Turtle()
-nindza.shape("turtle")
+nindza.shape("arrow")
 nindza.color("red")
-nindza.pensize(10)
-nindza.speed(5)
+nindza.pensize(2)
+nindza.speed(0)
 angles = [90,180,270,0]
 def random_color():
     r = random.randint(0,255)
     g = random.randint(0,255)
     b = random.randint(0,255)
     return (r,g,b)
-for _ in range(100):
-    nindza.right(random.choice(angles))
-    nindza.forward(30)
+heading = 0
+while heading <= 360:
+    nindza.circle(100)
+    nindza.setheading(heading)
     nindza.pencolor(random_color())
+    heading+=5
 
 screen = turtle.Screen()
 screen.exitonclick()
