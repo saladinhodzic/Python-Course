baa17d099e74185cd2950e8aa6876aca128a147d
saladinhodzic <hsaladin06@gmail.com>
2025-01-29 17:06:50 +0100

    Enhance turtle drawing by adding random movement and color changes

--- Day18/Day18.py
+++ Day18/Day18.py
@@ -1,15 +1,16 @@
 from turtle import Turtle,Screen
-
+import random
 nindza = Turtle()
 nindza.shape("turtle")
 nindza.color("red")
-
-for i in range(4,10):
-    for j in range(i):
-        nindza.forward(50)
-        nindza.right(360/i)
-    
-
+nindza.pensize(10)
+nindza.speed(5)
+angles = [90,180,270,360]
+colors = ["red","blue","yellow"]
+for _ in range(100):
+    nindza.right(random.choice(angles))
+    nindza.forward(30)
+    nindza.pencolor(random.choice(colors))
 
 screen = Screen()
 screen.exitonclick()
