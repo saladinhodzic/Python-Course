96ad901ac4b04e67ed6b56ea157111ca2f8429d4
saladinhodzic <hsaladin06@gmail.com>
2025-01-29 18:11:16 +0100

    Refactor turtle drawing to use random colors and improve angle selection

--- Day18/Day18.py
+++ Day18/Day18.py
@@ -1,16 +1,21 @@
-from turtle import Turtle,Screen
+import turtle 
 import random
-nindza = Turtle()
+turtle.colormode(255)
+nindza = turtle.Turtle()
 nindza.shape("turtle")
 nindza.color("red")
 nindza.pensize(10)
 nindza.speed(5)
-angles = [90,180,270,360]
-colors = ["red","blue","yellow"]
+angles = [90,180,270,0]
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    return (r,g,b)
 for _ in range(100):
     nindza.right(random.choice(angles))
     nindza.forward(30)
-    nindza.pencolor(random.choice(colors))
+    nindza.pencolor(random_color())
 
-screen = Screen()
+screen = turtle.Screen()
 screen.exitonclick()
