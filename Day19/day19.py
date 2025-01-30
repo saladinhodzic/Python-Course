8a80cb3de7d5206a783f6cf8479da5e58d53bd81
saladinhodzic <hsaladin06@gmail.com>
2025-01-30 20:45:53 +0100

    Add keyboard controls for turtle movement and clearing the screen

--- Day19/day19.py
+++ Day19/day19.py
@@ -2,8 +2,24 @@ from turtle import Turtle,Screen
 
 ninja = Turtle()
 screen = Screen()
-def move():
-    ninja.forward(10)
+def forward():
+    ninja.forward(20)
+def backward():
+    ninja.back(20)
+def clockwise():
+    current = ninja.heading()
+    ninja.setheading(current+10)
+def counter_clockwise():
+    current = ninja.heading()
+    ninja.setheading(current - 10)
+def clear():
+    ninja.setheading(0)
+    ninja.goto(0,0)
+    ninja.clear()
 screen.listen()
-screen.onkey(key="space", fun = move)
+screen.onkey(key="w", fun = forward)
+screen.onkey(key="s", fun = backward)
+screen.onkey(key="d", fun = clockwise)
+screen.onkey(key="a", fun = counter_clockwise)
+screen.onkey(key="c", fun = clear)
 screen.exitonclick()
