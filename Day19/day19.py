5e9bfdb0c28dd0ace3f16e07c0e56126139e886f
saladinhodzic <hsaladin06@gmail.com>
2025-01-30 21:14:06 +0100

    Add turtle race functionality with user betting and random movement

--- Day19/day19.py
+++ Day19/day19.py
@@ -1,9 +1,10 @@
 from turtle import Turtle,Screen
-
+import random
 screen = Screen()
 screen.setup(500,500)
 user_bet = screen.textinput(title="Enter your bet:",prompt="Enter color: ")
 colors = ['red','green','purple','yellow','orange','blue']
+turtles=[]
 y = -150
 for turtle in colors:
     new_turtle = Turtle()
@@ -12,4 +13,17 @@ for turtle in colors:
     new_turtle.color(turtle)
     new_turtle.goto(-230,y)
     y+=50
+    turtles.append(new_turtle)
+
+race_end = False
+while not race_end:
+    for turtle in turtles:
+        if turtle.xcor()>230:
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print("You won!")
+            else:
+                print("You lost!")
+            race_end = True
+        turtle.forward(random.randint(5,15))
 screen.exitonclick()
