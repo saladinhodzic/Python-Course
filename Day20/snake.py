2e3bdff3b85c4120fda83cd61fa613a4eb9f616d
saladinhodzic <hsaladin06@gmail.com>
2025-01-31 08:55:08 +0100

    Refactor snake game implementation to use a Snake class for better structure and encapsulation

--- Day20/snake.py
+++ Day20/snake.py
@@ -1,27 +1,19 @@
-from turtle import Turtle,Screen
-import time
-screen = Screen()
-screen.setup(600,600)
-screen.bgcolor("black")
-screen.title("Snake Game")
-screen.tracer(0)
-segments= []
-x=0
-for _ in range(3):
-    new_turtle = Turtle()
-    new_turtle.shape("square")
-    new_turtle.color("white")
-    new_turtle.penup()
-    new_turtle.setx(x)
-    x-=20
-    segments.append(new_turtle)
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    for turtle in range(len(segments)-1,0,-1):
-        new_x = segments[turtle-1].xcor()
-        new_y = segments[turtle-1].ycor()
-        segments[turtle].goto(new_x,new_y)
-    segments[0].forward(20)
-screen.exitonclick()
+import turtle
+class Snake():
+    def __init__(self):
+        self.tails= []
+        self.x = 0
+        for _ in range(3):
+            new_turtle = turtle.Turtle()
+            new_turtle.shape("square")
+            new_turtle.color("white")
+            new_turtle.penup()
+            new_turtle.setx(self.x)
+            self.x-=20
+            self.tails.append(new_turtle)
+    def move(self):
+        for turtle in range(len(self.tails)-1,0,-1):
+            new_x = self.tails[turtle-1].xcor()
+            new_y = self.tails[turtle-1].ycor()
+            self.tails[turtle].goto(new_x,new_y)
+        self.tails[0].forward(20)
