a5e99c2f07a4680a06317d9bca6801381cafceeb
saladinhodzic <hsaladin06@gmail.com>
2025-02-03 19:49:02 +0100

    Initialize car manager, player, and scoreboard classes; set up main game loop and screen

---
+++ Day23/main.py
@@ -0,0 +1,19 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+# making screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.listen()
+screen.tracer(0)
+
+# making player
+player = Player()
+screen.onkey(player.move,'space')
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.05)
+    screen.update()
