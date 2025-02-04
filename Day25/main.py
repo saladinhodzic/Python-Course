645a437ac0eacf2af7c60a0f6074fa9519ec6112
saladinhodzic <hsaladin06@gmail.com>
2025-02-04 08:22:28 +0100

    Add squirrel data processing and export fur color counts to CSV

--- Day25/main.py
+++ Day25/main.py
@@ -1,5 +1,13 @@
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data['temp'].mean())
-print(data['temp'].max())
+data = pandas.read_csv("squirrel_data.csv")
+
+colors = data["Primary Fur Color"].to_list()
+
+dict_data = {
+    "Fur Color":["Gray","Cinnamon","Black"],
+    "Count":[colors.count("Gray"),colors.count("Cinnamon"),colors.count("Black")]
+}
+
+new_data = pandas.DataFrame(dict_data)
+new_data.to_csv("data_sorted.csv")
