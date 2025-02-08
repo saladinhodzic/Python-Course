74a634d786c8780bc27c34207e280fb3d6e7947b
saladinhodzic <hsaladin06@gmail.com>
2025-02-08 08:24:56 +0100

    Add UI elements for password management including input fields and buttons

--- Day29/main.py
+++ Day29/main.py
@@ -11,8 +11,27 @@ window.config(padx=20,pady=20)
 logo = PhotoImage(file="logo.png")
 canvas = Canvas(height=200,width=200)
 canvas.create_image(100,100,image = logo)
-canvas.pack()
+canvas.grid(column=1,row=0)
 
+label_1= Label(text="Website: ")
+label_1.grid(column=0,row=1)
+input_1 = Entry(width=50)
+input_1.grid(column=1,row=1,columnspan=2)
+
+label_2 = Label(text="Email/Username: ")
+label_2.grid(column=0,row=2)
+input_2 = Entry(width=50)
+input_2.grid(column=1,row=2,columnspan=2)
+
+label_3 = Label(text="Password: ")
+label_3.grid(column=0,row=3)
+input_3 = Entry(width=25)
+input_3.grid(column=1,row=3)
+button_1 = Button(text="Generate Password")
+button_1.grid(column=2,row=3)
+
+button_2 = Button(text="Add",width=43)
+button_2.grid(column=1,row=4,columnspan=2)
 
 
 
