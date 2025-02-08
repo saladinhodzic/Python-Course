4aac62f63d9ad068b97e15e2e029891c60acba51
saladinhodzic <hsaladin06@gmail.com>
2025-02-08 09:10:35 +0100

    Finishing Password Manager Project

--- Day29/main.py
+++ Day29/main.py
@@ -1,7 +1,21 @@
 from tkinter import *
 from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+import random
+def generate():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    nr_letters = random.randint(8,10)
+    nr_symbols = random.randint(2,4)
+    nr_numbers = random.randint(2,4)
+    password=[random.choice(letters) for _ in range(nr_letters)]
+    password+=[random.choice(symbols) for _ in range(nr_symbols)]
+    password+=[random.choice(numbers) for _ in range(nr_numbers)]
+    random.shuffle(password)
 
+    new_pass = ''.join(password)
+    input_3.insert(0,new_pass)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def add_password():
@@ -44,7 +58,7 @@ label_3 = Label(text="Password: ")
 label_3.grid(column=0,row=3)
 input_3 = Entry(width=21)
 input_3.grid(column=1,row=3)
-button_1 = Button(text="Generate Password")
+button_1 = Button(text="Generate Password",command=generate)
 button_1.grid(column=2,row=3)
 
 button_2 = Button(text="Add",width=35,command=add_password)
