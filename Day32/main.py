eb4013f0246aef9c81b0af6bc39dee590babac1b
saladinhodzic <hsaladin06@gmail.com>
2025-02-13 12:38:47 +0100

    Add functionality to send a random quote via email on Wednesdays

--- Day32/main.py
+++ Day32/main.py
@@ -1,9 +1,19 @@
 import smtplib
+import datetime as dt
+import random
+
+now = dt.datetime.now()
+weekday = now.weekday()
 
 my_email = "testingpythoncode321@gmail.com"
 password = "wpkj fcii pimv gcfg"
 
-with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
-    connection.starttls()
-    connection.login(user=my_email,password=password)
-    connection.sendmail(from_addr=my_email,to_addrs="hsaladin06@gmail.com",msg="Hello World!")
+with open("quotes.txt") as quotes:
+    data = quotes.readlines()
+    random_quote = random.choice(data)
+
+if weekday == 3:
+    with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email,password=password)
+        connection.sendmail(from_addr=my_email,to_addrs="hsaladin06@gmail.com",msg=random_quote)
---
+++ Day32/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
+#     connection.starttls()
+#     connection.login(user=my_email,password=password)
+#     connection.
