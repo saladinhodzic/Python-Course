10eeb18afe424c07ae7110cdf2350b1b775170ba
saladinhodzic <hsaladin06@gmail.com>
2025-02-18 21:02:54 +0100

    Add sunrise and sunset time retrieval from API

---
+++ Day33/main.py
@@ -0,0 +1,27 @@
+import requests
+from datetime import datetime
+# LOCATION
+MY_LONGITUDE = 20.501587
+MY_LATITUDE = 43.160314
+
+# PARAMETERS
+
+parameters = {
+    "lat" : MY_LATITUDE,
+    "lng" : MY_LONGITUDE,
+    "formatted" : 0
+}
+
+# GET DATA FROM API
+
+response = requests.get("https://api.sunrise-sunset.org/json",params=parameters)
+response.raise_for_status()
+data = response.json()
+
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+
+# GET OUR CURRENT TIME
+
+time = datetime.now()
+print(time)
