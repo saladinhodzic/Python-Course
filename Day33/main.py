d543993bd4a50048bc28cd0004e1d8ffd3dbccdb
saladinhodzic <hsaladin06@gmail.com>
2025-02-18 21:11:19 +0100

    Extract hour from sunrise and sunset times for better readability

--- Day33/main.py
+++ Day33/main.py
@@ -18,9 +18,10 @@ response = requests.get("https://api.sunrise-sunset.org/json",params=parameters)
 response.raise_for_status()
 data = response.json()
 
-sunrise = data["results"]["sunrise"]
-sunset = data["results"]["sunset"]
-
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+print(sunrise)
+print(sunset)
 # GET OUR CURRENT TIME
 
 time = datetime.now()
