2543782e68ddfc9bd8a6a462730293e3f363f978
saladinhodzic <hsaladin06@gmail.com>
2025-02-20 23:07:53 +0100

    Implement User Interface for quiz application using Tkinter

--- Day34/quizzler-app-start/ui.py
+++ Day34/quizzler-app-start/ui.py
@@ -1,2 +1,8 @@
 THEME_COLOR = "#375362"
-
+from tkinter import *
+class UserInterface():
+    
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quiz")
+        self.window.mainloop()
