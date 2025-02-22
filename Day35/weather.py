c42765908b141aa327ab77168bee4665d4425c96
saladinhodzic <hsaladin06@gmail.com>
2025-02-22 14:00:19 +0100

    Enhance weather API integration to limit forecast data to 4 days and provide umbrella suggestion based on weather conditions

--- Day35/weather.py
+++ Day35/weather.py
@@ -3,7 +3,12 @@ import requests
 MY_LONGITUDE = 20.501587
 MY_LATITUDE = 43.160314
 API_KEY = "bd5e378503939ddaee76f12ad7a97608"
-URL = f"https://api.openweathermap.org/data/2.5/forecast?lat={MY_LATITUDE}&lon={MY_LONGITUDE}&appid={API_KEY}"
+URL = f"https://api.openweathermap.org/data/2.5/forecast?lat={MY_LATITUDE}&lon={MY_LONGITUDE}&appid={API_KEY}&cnt=4"
 
 response = requests.get(URL)
-print(response)
+response.raise_for_status()
+# get the weather data
+data = response.json()
+for day in data["list"]:
+    if day["weather"][0]["id"] < 800:
+        print("Bring an umbrella")
