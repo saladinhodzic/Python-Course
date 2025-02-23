6b0d3067aec8afc192358e9e5fa905bc5d6cc5ca
saladinhodzic <hsaladin06@gmail.com>
2025-02-23 15:31:43 +0100

    Using Twilio API to send SMS

--- Day35/weather.py
+++ Day35/weather.py
@@ -1,10 +1,7 @@
 import requests
 from twilio.rest import Client
 
-ACCOUNT_SID = "AC2f8475f8b617394a89cab6c7c055e97f"
-AUTH_TOKEN = "7f9b9c7aaf6ebffb46d8a43cb31f8e80"
-MY_LONGITUDE = 20.501587
-MY_LATITUDE = 43.160314
+
 API_KEY = "bd5e378503939ddaee76f12ad7a97608"
 URL = f"https://api.openweathermap.org/data/2.5/forecast?lat={MY_LATITUDE}&lon={MY_LONGITUDE}&appid={API_KEY}&cnt=4"
 
