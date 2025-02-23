18a0e7648bf2e849f6fe2ceda7690485bd74a724
saladinhodzic <hsaladin06@gmail.com>
2025-02-23 15:43:43 +0100

    Using Twilio API to send SMS

--- Day35/weather.py
+++ Day35/weather.py
@@ -1,7 +1,8 @@
 import requests
 from twilio.rest import Client
 
-
+MY_LONGITUDE = 20.501587
+MY_LATITUDE = 43.160314
 API_KEY = "bd5e378503939ddaee76f12ad7a97608"
 URL = f"https://api.openweathermap.org/data/2.5/forecast?lat={MY_LATITUDE}&lon={MY_LONGITUDE}&appid={API_KEY}&cnt=4"
 
@@ -20,5 +21,4 @@ if will_rain:
     message = client.messages \
         .create(
             body="It's going to rain today.",
-            
         )
