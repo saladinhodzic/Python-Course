a8d65ba4a5aeec74d87b76805df8818ffd063549
saladinhodzic <hsaladin06@gmail.com>
2025-02-27 20:24:18 +0100

    Refactor user profile creation by extracting constants and adding graph and pixel creation functionality

--- Day37/main.py
+++ Day37/main.py
@@ -1,13 +1,39 @@
 import requests
-
+USERNAME = "saladin"
+TOKEN = "asbd1yw91hrsjfdb19"
 create_user_params = {
-    "token" : "asbd1yw91hrsjfdb19",
-    "username": "saladin",
+    "token" : TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor" : "yes"
 }
 
 url = "https://pixe.la/v1/users"
 
-response = requests.post(url,json=create_user_params)
-print(response.text)
+# response = requests.post(url,json=create_user_params)
+# print(response.text)
+
+graph_endpoint = f"{url}/{USERNAME}/graphs"
+
+graph_config = {
+    "id":"sakkka1",
+    "name":"Studying",
+    "unit":"hours",
+    "type":"float",
+    "color":"kuro"
+}
+
+headers = {
+    "X-USER-TOKEN":TOKEN
+}
+
+# make_graph = requests.post(graph_endpoint,json=graph_config,headers=headers)
+
+pixel_endpoint = f"{url}/{USERNAME}/graphs/sakkka1"
+
+pixel_config = {
+    "date" : "20250227",
+    "quantity": "1.1"
+}
+
+post_pixel = requests.post(url = pixel_endpoint,json=pixel_config, headers = headers)
