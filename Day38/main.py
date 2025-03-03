6e913863dae6ecae5c58e4c189aa5c9ea0236896
saladinhodzic <hsaladin06@gmail.com>
2025-03-03 12:51:31 +0100

    Add date and time formatting for exercise tracking and update Sheety API integration

--- Day38/main.py
+++ Day38/main.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from datetime import datetime
 APP_ID = os.getenv("APP_ID")
 API_KEY = os.getenv("API_KEY")
 URL = "https://trackapi.nutritionix.com/v2/natural/exercise"
@@ -15,7 +16,21 @@ response = requests.post(url=URL,json={"query":query},headers=HEADERS)
 data = response.json()
 duration = data["exercises"][0]['duration_min']
 calories = data["exercises"][0]['nf_calories']
-exercise = data["exercises"][0]["name"]
+exercise = data["exercises"][0]["name"].title()
+today = datetime.now().date()
+time = datetime.now().time()
 # SHEETY API ENDPOINT
-
 sheety_api = "https://api.sheety.co/b72a2bb486406ba3a1d6c186930e9930/vezbanje/workouts"
+sheety_data = {
+    "workout":
+    {
+        "date":today.strftime("%d/%m/%Y"),
+        "time": time.strftime("%H:%M:%S"),
+        "exercise":exercise,
+        "duration":duration,
+        "calories": calories
+    }
+}
+
+sheety_response = requests.post(url=sheety_api,json=sheety_data)
+print(sheety_response.text)
