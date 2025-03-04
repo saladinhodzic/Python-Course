ef60693005ba136468baed534ffdabaa6787d34e
saladinhodzic <hsaladin06@gmail.com>
2025-03-04 08:12:46 +0100

    Implement flight search functionality using Amadeus API

--- Day39/flight-deals-start/main.py
+++ Day39/flight-deals-start/main.py
@@ -1,12 +1,18 @@
 import requests
+from datetime import datetime,timedelta
 from flight_search import FlightSearch
-from pprint import pprint
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 
 URL = "https://api.sheety.co/b72a2bb486406ba3a1d6c186930e9930/avioJeftinaPutovanja/prices"
 response = requests.get(url=URL)
+sheet_data = response.json()["prices"]
 
 get_flights = FlightSearch()
 
+city_origin = "LON"
 
-sheet_data = response.json()["prices"]
+tommorow = datetime.now() + timedelta(days=1)
+
+for city in sheet_data:
+    response = get_flights.search_flights(city_origin,city["iataCode"],from_time=tommorow)
+    print(response)
