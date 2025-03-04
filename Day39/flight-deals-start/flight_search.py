45fbfa5f1913d8d080283709054e1a50dae1f73e
saladinhodzic <hsaladin06@gmail.com>
2025-03-04 06:57:10 +0100

    Add weather notification feature and flight search API integration

--- Day39/flight-deals-start/flight_search.py
+++ Day39/flight-deals-start/flight_search.py
@@ -1,3 +1,23 @@
+import os
+from dotenv import load_dotenv
+import requests
+load_dotenv()
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self):
+        self.api_key = os.getenv("AMADEUS_KEY")
+        self.api_secret = os.getenv("API_SECRET")
+        self.token = self.get_new_token()
+    
+    def get_new_token(self):
+        self.headers = {
+            "Content-Type" : "application/x-www-form-urlencoded"
+        }
+        self.body = {
+            "grant_type" : "client_credentials",
+            "client_id" : self.api_key,
+            "client_secret" : self.api_secret
+        }
+        
+        response = requests.post(url="https://test.api.amadeus.com/v1/security/oauth2/token",headers=self.headers,data=self.body)
+        return response.json()["access_token"]
--- Day39/flight-deals-start/main.py
+++ Day39/flight-deals-start/main.py
@@ -1,8 +1,12 @@
 import requests
+from flight_search import FlightSearch
 from pprint import pprint
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 
 URL = "https://api.sheety.co/b72a2bb486406ba3a1d6c186930e9930/avioJeftinaPutovanja/prices"
 response = requests.get(url=URL)
+
+get_flights = FlightSearch()
+
+
 sheet_data = response.json()["prices"]
-pprint(sheet_data)
