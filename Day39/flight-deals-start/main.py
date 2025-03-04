6a6d5267d71bb270bda4b02a920456c56d880704
saladinhodzic <hsaladin06@gmail.com>
2025-03-04 06:24:12 +0100

    Add API request to Sheety for flight prices and print response

--- Day39/flight-deals-start/main.py
+++ Day39/flight-deals-start/main.py
@@ -1,1 +1,7 @@
+import requests
+from pprint import pprint
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+URL = "https://api.sheety.co/b72a2bb486406ba3a1d6c186930e9930/avioJeftinaPutovanja/prices"
+response = requests.get(url=URL)
+pprint(response.json())
