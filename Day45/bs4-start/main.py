f4e1f93a8b5c803542367716004d48d30fb30bdf
saladinhodzic <hsaladin06@gmail.com>
2025-03-13 06:20:33 +0100

    Replace local HTML file reading with requests to fetch live data from Hacker News

--- Day45/bs4-start/main.py
+++ Day45/bs4-start/main.py
@@ -1,5 +1,11 @@
 from bs4 import BeautifulSoup
-with open("./website.html") as site:
-    content = site.read()
-soup = BeautifulSoup(content,features="html.parser")
-print(soup.prettify())
+import requests
+# with open("./website.html") as site:
+#     content = site.read()
+# soup = BeautifulSoup(content,features="html.parser")
+# print(soup.prettify())
+response = requests.get("https://news.ycombinator.com/")
+html = response.text
+soup = BeautifulSoup(html,"html.parser")
+anchor_tags = soup.select(".titleline a")
+print(anchor_tags)    
