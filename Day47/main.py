a8b807abcd9eb6cacc2baa107a8b0f33c7e13331
saladinhodzic <hsaladin06@gmail.com>
2025-03-14 07:14:45 +0100

    Add email notification for Instant Pot price drop and update .gitignore for environment variables

--- Day47/main.py
+++ Day47/main.py
@@ -1,9 +1,20 @@
+import os
 import requests
+import smtplib
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+
+# getting the enviroment variables
+load_dotenv()
 
 response = requests.get("https://appbrewery.github.io/instant_pot/")
 html = response.text
 soup = BeautifulSoup(html,"html.parser")
-price = soup.select_one(".a-price-whole").getText()
+whole = soup.select_one(".a-price-whole").getText()
 decimal = soup.select_one(".a-price-fraction").getText()
-print(float(price+decimal))
+price = float(whole+decimal)
+if price < 100:
+    with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
+        connection.starttls()
+        connection.login(user=os.getenv("SENDER_MAIL"),password=os.getenv("SENDER_PASS"))
+        connection.sendmail(from_addr=os.getenv("SENDER_MAIL"),to_addrs=os.getenv("MY_MAIL"),msg="Subject:Alert! Price went down!\n\nThe price of the pot went below 100$")
