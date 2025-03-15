cba4bb52a96daa45f58292983224fb8c58c7f76f
saladinhodzic <hsaladin06@gmail.com>
2025-03-15 12:42:07 +0100

    Implement web scraping for Python events using Selenium in main.py

--- Day48/main.py
+++ Day48/main.py
@@ -1,5 +1,12 @@
 # USING SELENIUM FOR WEB SCRAPING
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 options = webdriver.ChromeOptions().add_experimental_option("detach",True)
 driver = webdriver.Chrome(options=options)
-driver.get("https://kupujemprodajem.com")
+driver.get("https://www.python.org/")
+menu = driver.find_elements(By.CSS_SELECTOR,'.event-widget li')
+dict = {}
+for li in menu:
+    key,value = li.text.split("\n")
+    dict[key] = value
+print(dict)
