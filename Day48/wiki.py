af7fe3f4f522e04831d9ac13b2779f4af8fb96dc
saladinhodzic <hsaladin06@gmail.com>
2025-03-15 13:10:22 +0100

    Refactor Selenium options setup and add search functionality for Python on Wikipedia

--- Day48/wiki.py
+++ Day48/wiki.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-options = webdriver.ChromeOptions().add_experimental_option("detach",True)
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach",True)
 driver = webdriver.Chrome(options=options)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 articles = driver.find_element(By.XPATH,'//*[@id="articlecount"]/ul/li[2]/a[1]')
-print(articles.text)
+button = driver.find_element(By.CLASS_NAME,'search-toggle').click()
+input = driver.find_element(By.NAME,"search")
+input.send_keys("Python")
