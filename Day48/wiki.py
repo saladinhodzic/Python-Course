8822b5648f7944b99460f44cab8f700fe7ed9e00
saladinhodzic <hsaladin06@gmail.com>
2025-03-15 13:22:50 +0100

    Update Selenium script to automate form submission on secure-retreat website

--- Day48/wiki.py
+++ Day48/wiki.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach",True)
 driver = webdriver.Chrome(options=options)
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
-articles = driver.find_element(By.XPATH,'//*[@id="articlecount"]/ul/li[2]/a[1]')
-button = driver.find_element(By.CLASS_NAME,'search-toggle').click()
-input = driver.find_element(By.NAME,"search")
-input.send_keys("Python")
+driver.get("https://secure-retreat-92358.herokuapp.com/")
+input = driver.find_element(By.NAME,"fName")
+input.send_keys("Saki",Keys.ENTER)
+lname = driver.find_element(By.NAME,"lName")
+lname.send_keys("Hodza",Keys.ENTER)
+email = driver.find_element(By.NAME,"email")
+email.send_keys("Saki@gmail.com",Keys.ENTER)
+button = driver.find_element(By.CLASS_NAME,'.form-signin button').click()
