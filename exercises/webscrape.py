81c697a434e6112243422e157c2eb247720e5390
saladinhodzic <hsaladin06@gmail.com>
2025-03-19 13:53:13 +0100

    Add search functionality to webscrape.py using Selenium

--- exercises/webscrape.py
+++ exercises/webscrape.py
@@ -1,6 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 # opening the webpage using Selenium
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach",True)
 driver = webdriver.Chrome(options=options)
 driver.get("https://google.com")
+# searching on the google
+try:
+    textarea = driver.find_element(By.CLASS_NAME,"gLFyf")
+    textarea.send_keys("Python Selenium",Keys.ENTER)
+except Exception as e:
+    print(f"Failed to find textarea object {e}")
