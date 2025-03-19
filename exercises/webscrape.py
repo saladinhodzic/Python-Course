656372ddd7c9799fe2c32b38e899450dc9cc5f91
saladinhodzic <hsaladin06@gmail.com>
2025-03-19 14:05:03 +0100

    Update webscrape.py to navigate to YouTube and interact with Shorts feature

--- exercises/webscrape.py
+++ exercises/webscrape.py
@@ -1,14 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 # opening the webpage using Selenium
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach",True)
 driver = webdriver.Chrome(options=options)
-driver.get("https://google.com")
+driver.get("https://youtube.com")
 # searching on the google
 try:
-    textarea = driver.find_element(By.CLASS_NAME,"gLFyf")
-    textarea.send_keys("Python Selenium",Keys.ENTER)
+    shorts = driver.find_element(By.CSS_SELECTOR,"a[title='Shorts']")
+    shorts.click()
+    time.sleep(10)
+    # returning back to the main page
+    driver.back()
 except Exception as e:
     print(f"Failed to find textarea object {e}")
