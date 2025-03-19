1263dbb4b140ee0e4c53ba7b1ac56d32b2a021f2
saladinhodzic <hsaladin06@gmail.com>
2025-03-19 11:32:13 +0100

    Implement InternetSpeedBot to measure and report internet speed

--- Day50/main.py
+++ Day50/main.py
@@ -1,3 +1,6 @@
 # Creating Twitter Complaint bot
+from utils import InternetSpeedBot
 PROMISED_DOWN = 150
 PROMISED_UP = 30
+speed_bot = InternetSpeedBot()
+speed_bot.get_internet_speed()
--- Day50/utils.py
+++ Day50/utils.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 class InternetSpeedBot():
     def __init__(self):
         self.up = 150
         self.down = 10
         self.driver = webdriver.Chrome()
     def get_internet_speed(self):
-        pass
+        self.options = webdriver.ChromeOptions()
+        self.options.add_experimental_option("detach",True)
+        self.driver.get("https://www.speedtest.net/")
+        self.start = self.driver.find_element(By.CLASS_NAME,"js-start-test").click()
+        time.sleep(40)
+        self.download = self.driver.find_element(By.CLASS_NAME,"download-speed").text
+        self.upload = self.driver.find_element(By.CLASS_NAME,"upload-speed").text
+        print(f"Download: {self.download}\nUpload: {self.upload}")
     def tweet(self):
         pass
-speed_bot = InternetSpeedBot()
-speed_bot.get_internet_speed()
-speed_bot.tweet()
