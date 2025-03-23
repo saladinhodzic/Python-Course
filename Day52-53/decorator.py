39413a8a8601629f58e5a580e3b9dab989af5e71
saladinhodzic <hsaladin06@gmail.com>
2025-03-23 15:38:59 +0100

    Implement user authentication check in decorator and update post creation function

--- Day52-53/decorator.py
+++ Day52-53/decorator.py
@@ -1,12 +1,27 @@
 # Making decorator function
-import time
-def decorator(function):
-    def wrapper():
-        time.sleep(2)
-        function()
-    return wrapper
-@decorator
-def say_hello():
-    print("Hello")
 
-say_hello()
+def decorator(function):
+    def wrapper(*args,**kwargs):
+        if args[0].is_logged_in:
+            function(args[0])
+    return wrapper
+class User():
+    def __init__(self,name):
+        self.name = name
+        self.is_logged_in = False
+user = User("saki")
+user.is_logged_in = True
+@decorator
+def create_post(user):
+    print(f"{user.name} created the post")
+create_post(user)
+# def decorator(function):
+#     def wrapper():
+#         time.sleep(2)
+#         function()
+#     return wrapper
+# @decorator
+# def say_hello():
+#     print("Hello")
+
+# say_hello()
