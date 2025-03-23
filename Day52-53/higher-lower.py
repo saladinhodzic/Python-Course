2ba147cb40b8cc77c0f523edfcf0e29f7bc6611b
saladinhodzic <hsaladin06@gmail.com>
2025-03-23 16:01:43 +0100

    Finishing building the number guessing game using Flask

--- Day52-53/higher-lower.py
+++ Day52-53/higher-lower.py
@@ -12,10 +12,10 @@ def start_game():
 @app.route("/<int:guess>")
 def check(guess):
     if guess < random_number:
-        return '<h1>Too low! Try higher!</h1>'
+        return '<h1>Too low! Try higher!</h1><img src="https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif"/>'
     elif guess > random_number:
-        return '<h1>Too high! Try lower!</h1>'
+        return '<h1>Too high! Try lower!</h1><img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif"/>'
     else:
-        return '<h1>You got it!</h1>'
+        return '<h1>You got it!</h1><img src="https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif"/>'
 if __name__ == "__main__":
     app.run(debug=True)
