3042539b2e3833a70ff7451f7072ca8039d15c21
saladinhodzic <hsaladin06@gmail.com>
2025-03-23 15:54:34 +0100

    Add Flask application for "Guess the Number" game

---
+++ Day52-53/higher-lower.py
@@ -0,0 +1,11 @@
+# Creating Guess the Number game using Flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def guess():
+    return '<h1>Guess the number between 0 and 9</h1><img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif"/>'
+
+if __name__ == "__main__":
+    app.run(debug=True)
