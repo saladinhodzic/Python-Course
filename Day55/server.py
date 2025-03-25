3c53ac5dbc941d9c8e4c64cf7bea7c429d5ca88c
saladinhodzic <hsaladin06@gmail.com>
2025-03-25 16:08:02 +0100

    Update Flask app to use gender prediction API and modify index.html for personalized greetings

--- Day55/server.py
+++ Day55/server.py
@@ -1,12 +1,13 @@
 from flask import Flask,render_template
-import random
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    random_number = random.randint(0,10)
-    return render_template("index.html",random_number=random_number)
+@app.route("/<name>")
+def hello(name):
+    response = requests.get(f"https://api.genderize.io?name={name}")
+    data=response.json()
+    return render_template("index.html",name = data["name"],gender=data['gender'],percentage=data['probability'])
 
 if __name__ == "__main__":
     app.run(debug=True)
