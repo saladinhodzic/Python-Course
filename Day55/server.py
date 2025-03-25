aa1485104ec6276d0ad581b63b7879409e1ab0d0
saladinhodzic <hsaladin06@gmail.com>
2025-03-25 15:51:52 +0100

    Introducing jinja markup in Python

---
+++ Day55/server.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    random_number = random.randint(0,10)
+    return render_template("index.html",random_number=random_number)
+
+if __name__ == "__main__":
+    app.run(debug=True)
