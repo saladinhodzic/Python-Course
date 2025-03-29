f25a9600294ffcc006fe2dfc4ee5a76fd0b69ee8
saladinhodzic <hsaladin06@gmail.com>
2025-03-29 22:37:16 +0100

    Add login form to index and implement login route in Flask

--- Day58/main.py
+++ Day58/main.py
@@ -1,11 +1,15 @@
 # HTML Forms - catching submitted form via Flask server
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login",methods=["POST"])
+def login():
+    return f"{request.form["name"]}:{request.form["password"]}"
 
 if __name__ == "__main__":
     app.run(debug=True)
