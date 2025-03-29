ea9b073f364803f34429a4e3bde631199ee3ce53
saladinhodzic <hsaladin06@gmail.com>
2025-03-29 22:45:15 +0100

    Set up Flask application with login functionality and multiple routes; add templates and requirements

---
+++ Day58/day-60-starting-files-blog-with-contact-form/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template,request
+import requests
+
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+@app.route("/login",methods=["POST"])
+def recieve_data():
+    print(f"{request.form["name"]},{request.form["email"]},{request.form["phone"]},{request.form["message"]}")
+    return "<h1>You successfully logged in</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
