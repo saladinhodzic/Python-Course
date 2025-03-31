142b01628f81de459cc24074b432dc89aa228d4b
saladinhodzic <hsaladin06@gmail.com>
2025-04-01 00:00:07 +0200

    Add form handling to login route and update login template

--- Day59/day-61-starting-files-flask-secrets/main.py
+++ Day59/day-61-starting-files-flask-secrets/main.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
 '''
 Red underlines? Install the required packages first: 
 Open the Terminal in PyCharm (bottom left). 
@@ -15,13 +20,16 @@ This will install the packages from requirements.txt for this project.
 
 
 app = Flask(__name__)
+app.secret_key = "Hello"
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    my_form = MyForm()
+    return render_template("login.html",form = my_form)
 if __name__ == '__main__':
     app.run(debug=True)
