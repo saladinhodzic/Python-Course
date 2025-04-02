6d607fb0717a5f09d0fe749e68fabcd9f01c0777
saladinhodzic <hsaladin06@gmail.com>
2025-04-02 23:47:05 +0200

    Add article management functionality to main.py

--- requisition/main.py
+++ requisition/main.py
@@ -5,4 +5,20 @@ month = date.split("-")[1]
 day = date.split("-")[2]
 
 pattern = f"PRM-{day}{month}-ODR290"
-print(pattern)
+
+articles = {}    
+
+def add_articles(local):
+    needs = input(f"What do you need for {local}: ")
+    if needs:
+        if local.title() not in articles.keys():
+            quantity = input(f"How much of {needs} you need: ")
+            articles[local.title()] = {needs:quantity}
+            return add_articles(local)
+        quantity = input(f"How much of {needs} you else need: ")
+        articles[local.title()].update({needs:quantity})
+        return add_articles(local)
+for _ in range(2):
+    local = input("Enter the local name: ")
+    add_articles(local)
+print(articles)
