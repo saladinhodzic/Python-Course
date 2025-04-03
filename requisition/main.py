cca2603778573bee1be649053368bdb2360e9620
saladinhodzic <hsaladin06@gmail.com>
2025-04-03 15:31:22 +0200

    Refactor article addition logic and enhance output formatting in main.py

--- requisition/main.py
+++ requisition/main.py
@@ -4,21 +4,25 @@ date = str(datetime.now().date())
 month = date.split("-")[1]
 day = date.split("-")[2]
 
-pattern = f"PRM-{day}{month}-ODR290"
+pattern = f"PRM-{day}{month}-ODR290\n"
 
-articles = {}    
+articles = {}
 
 def add_articles(local):
-    needs = input(f"What do you need for {local}: ")
+    needs = input(f"What do you need for {local}: ").title()
     if needs:
-        if local.title() not in articles.keys():
+        if local not in articles.keys():
             quantity = input(f"How much of {needs} you need: ")
-            articles[local.title()] = {needs:quantity}
+            articles[local] = {needs:quantity}
             return add_articles(local)
         quantity = input(f"How much of {needs} you else need: ")
-        articles[local.title()].update({needs:quantity})
+        articles[local].update({needs:quantity})
         return add_articles(local)
 for _ in range(2):
-    local = input("Enter the local name: ")
+    local = input("Enter the local name: ").title()
     add_articles(local)
-print(articles)
+    pattern += f"{local}:\n"
+    for item,quantity in articles[local].items():
+        pattern+=f"{quantity}x {item}\n"
+
+print(pattern)
