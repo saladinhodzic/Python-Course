2312286a97327ee4edf8d6a6060dc5c8aca010ea
saladinhodzic <hsaladin06@gmail.com>
2025-04-05 00:08:45 +0200

    Add form handling and new input fields for article submission in Flask app

--- requisition/server.py
+++ requisition/server.py
@@ -6,7 +6,10 @@ articles = {}
 
 @app.route("/",methods=["POST","GET"])
 def home():
+    if request.method == "POST":
+        articles[request.form.get("local")] = f"{request.form.get("kolicina")} {request.form.get("vrsta")} {request.form.get("artikl")}"
+        print(articles)
+        
     return render_template("index.html")
-
 if __name__ == "__main__":
     app.run(debug=True)
