3a9fa71ef08a8805469533a88095906fe563e019
saladinhodzic <hsaladin06@gmail.com>
2025-04-05 21:10:46 +0200

    Refactor article handling in Flask app and update index.html to display articles

--- requisition/server.py
+++ requisition/server.py
@@ -7,9 +7,9 @@ articles = {}
 @app.route("/",methods=["POST","GET"])
 def home():
     if request.method == "POST":
-        articles[request.form.get("local")] = f"{request.form.get("kolicina")} {request.form.get("vrsta")} {request.form.get("artikl")}"
-        print(articles)
-        
-    return render_template("index.html")
+        articles[request.form.get("local")] = {f"{request.form.get("kolicina")} {request.form.get("vrsta")} {request.form.get("artikl")}"}
+        for article in articles:
+            print(article)
+    return render_template("index.html",articles=articles)
 if __name__ == "__main__":
     app.run(debug=True)
