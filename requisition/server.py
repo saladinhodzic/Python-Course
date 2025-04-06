ec69b628c0c9f5e01f87e47cf8bf287a2affe953
saladinhodzic <hsaladin06@gmail.com>
2025-04-06 18:56:04 +0200

    Refactor server.py and premija.html to enhance order generation and update location options

--- requisition/server.py
+++ requisition/server.py
@@ -1,5 +1,10 @@
 from flask import Flask,render_template,request
+from datetime import datetime,timedelta
 
+date = str(datetime.now().date() + timedelta(days=1))
+month = date.split("-")[1]
+day = date.split("-")[2]
+premija_pattern = f"PRM-{day}{month}-ORD456"
 app = Flask(__name__)
 
 articles = {}
@@ -11,13 +16,13 @@ def home():
 @app.route("/premija",methods=["POST","GET"])
 def premija():
     if request.method == "POST":
-        local = request.form.get("local")
-        artikl = request.form.get("artikl")
+        local = request.form.get("local").title()
+        artikl = request.form.get("artikl").title()
         kolicina = request.form.get("kolicina")
         vrsta = request.form.get("vrsta")
         if local not in articles:
             articles[local] = {}
         articles[local][artikl] = f"{kolicina}x {vrsta}"
-    return render_template("premija.html",articles=articles)
+    return render_template("premija.html",articles=articles,order = premija_pattern)
 if __name__ == "__main__":
     app.run(debug=True)
