a89e741a2d8b5722214bf4b2dabc45ef9d244e0c
saladinhodzic <hsaladin06@gmail.com>
2025-04-10 15:31:08 +0200

    Update order number in premija_order.txt, implement user authentication in server.py, and add logout link in index.html

--- requisition/server.py
+++ requisition/server.py
@@ -1,9 +1,16 @@
 from flask import Flask,render_template,request,redirect,url_for
+from flask_login import LoginManager,UserMixin,login_user,login_required,logout_user,current_user
 from datetime import datetime,timedelta
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,PasswordField
 from wtforms.validators import DataRequired
 
+# user class
+class User(UserMixin):
+    def __init__(self,id):
+        self.id = id
+
+# form
 class MyForm(FlaskForm):
     input = StringField("Ime radnje: ",validators=[DataRequired("Obavezan unos")])
     password = PasswordField("Unesite lozinku: ",validators=[DataRequired("Obavezan unos")])
@@ -19,16 +26,25 @@ with open("premija_order.txt",'r+') as order:
     order.seek(0)
     order.write(str(ord))
     
-    
+# settings for app
 app = Flask(__name__)
 app.secret_key = "sendvicgalerija"
+login_manager = LoginManager()
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(id):
+    return User(id)
+
 articles = {}
 
 @app.route("/")
 def home():
-    return redirect(url_for('auth'))
+    if not current_user.is_authenticated:
+        return redirect(url_for('auth'))
+    return render_template('index.html')
 
 @app.route("/premija",methods=["POST","GET"])
+@login_required
 def premija():
     if request.method == "POST":
         local = request.form.get("local").title()
@@ -44,7 +60,14 @@ def auth():
     form = MyForm()
     if form.validate_on_submit() == True:
         if form.input.data == "saladin" and form.password.data == "123456":
-            return render_template("index.html")
-    return render_template("auth.html", form= form)
+            user = User(id="saladin")
+            login_user(user)
+            return redirect(url_for("home"))
+    return render_template("auth.html", form = form)
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
 if __name__ == "__main__":
     app.run(debug=True)
