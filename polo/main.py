e495dfbaef58f3e6b68cbc4a029b92102e56d74d
saladinhodzic <hsaladin06@gmail.com>
2025-05-24 14:06:52 +0200

    Refactor city search logic to improve email notification for Volkswagen Polo listings in Novi Pazar with price details

--- polo/main.py
+++ polo/main.py
@@ -11,9 +11,12 @@ driver = webdriver.Chrome(options=options)
 driver.get("https://www.polovniautomobili.com/auto-oglasi/pretraga?brand=volkswagen&model%5B%5D=polo&price_to=1900&year_from=2002&year_to=&region%5B%5D=2561&showOldNew=all&submit_1=&without_price=1")
 
 polo_cities = driver.find_elements(By.CLASS_NAME,"city")
-for city in polo_cities:
-    if city.text == 'Novi Pazar':
+polos = driver.find_elements(By.TAG_NAME,'article')
+for index in range(0,len(polos)-1):
+    print(polo_cities[index].text)
+    if polo_cities[index].text == 'Novi Pazar':
+        price = driver.find_elements(By.CSS_SELECTOR,".price span")[index].text.split(" ")[0]
         with SMTP("smtp.gmail.com",port=587) as connection:
             connection.starttls()
             connection.login(user = os.getenv('SENDER_MAIL'),password=os.getenv("SENDER_PASS"))
-            connection.sendmail(from_addr=os.getenv("SENDER_MAIL"),to_addrs=os.getenv("MY_MAIL"),msg="Subject: Polo u Pazaru\n\nPronadjen dobar oglas za polo koji trazis")
+            connection.sendmail(from_addr=os.getenv("SENDER_MAIL"),to_addrs=os.getenv("MY_MAIL"),msg=f"Subject: Polo u Pazaru\n\nPronadjen dobar oglas za polo koji trazis po ceni od {price}e")
