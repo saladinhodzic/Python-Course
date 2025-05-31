8f19375b148d1bfeb6fc87de0fcbbe327f89dd75
saladinhodzic <hsaladin06@gmail.com>
2025-05-31 19:56:00 +0200

    Add login form to index.html and implement route for data reception

--- Day58/html-forms/main.py
+++ Day58/html-forms/main.py
@@ -1,10 +1,11 @@
 from flask import Flask,render_template
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('index.html')
-
+@app.route("/login",methods = ["POST"])
+def recieve_data():
+    pass
 if __name__ == "__main__":
     app.run(debug=True)
